3c1dd7ca72dd0e938e0195022fada7a3c6e5a8a1
Dera41397 <122143675+Dera41397@users.noreply.github.com>
2023-02-08 06:14:17 +0900

    make_model.py up 3rd

--- make_model.py
+++ make_model.py
@@ -18,7 +18,7 @@ from keras.models import Model
 data_file = "./images\ETL8G.pickle"
 im_size = 32
 out_size = 12075 # あーんまでの文字の数
-im_color = 1
+im_color = 3
 in_shape = (im_size, im_size, im_color)
 
 # 保存した画像データ一覧を読み込む
@@ -38,11 +38,13 @@ y = np.array(y)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_size=0.8, shuffle=True)
 
 # vgg16のインスタンスの生成
-input_tensor = Input(shape=(32, 32, 1))
+input_tensor = Input(shape=(32, 32, 3))
 vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
+
 
 #モデル構築
 top_model = Sequential()
+'''
 top_model.add(Conv2D(32, (3, 3), input_shape=in_shape))
 top_model.add(BatchNormalization())
 top_model.add(Activation('relu'))
@@ -65,6 +67,12 @@ top_model.add(Activation('relu'))
 top_model.add(Dropout(0.5))
 top_model.add(Dense(out_size))
 top_model.add(Activation('softmax'))
+'''
+
+top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(2, activation='softmax'))
 
 # モデルの連結
 model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
@@ -73,17 +81,29 @@ model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
 for layer in model.layers[:19]:
     layer.trainable = False
 
-model.compile(loss='categorical_crossentropy',
+top_model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
               metrics=['accuracy'])
 
 
-hist = model.fit(
+hist = top_model.fit(
   x_train, y_train,
   batch_size=64, epochs=50,verbose=1,
   validation_data=(x_test, y_test))
 
-score = model.evaluate(x_test, y_test, verbose=1)
+score = top_model.evaluate(x_test, y_test, verbose=1)
 print("正解率 ", score[1], "loss ", score[0])
 
-model.save('ETL8-model.h5')
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('Accuracy')
+plt.legend(['train', 'test'], loc = 'upper left')
+plt.show()
+
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('Loss')
+plt.legend(['train', 'test'], loc = 'upper left')
+plt.show()
+
+top_model.save('ETL8-model.h5')
