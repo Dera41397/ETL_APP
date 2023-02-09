ba462a120dd0426bbdf559cdbee3176bdb45d666
Dera41397 <122143675+Dera41397@users.noreply.github.com>
2023-02-09 10:22:18 +0900

    add Images file 4th

--- make_model.py
+++ make_model.py
@@ -8,17 +8,13 @@ from keras.optimizers import RMSprop
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.normalization.batch_normalization import BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Input
-from keras.applications.vgg16 import VGG16
-from keras import optimizers
-from keras.models import Model
 
 
 # データファイルと画像サイズの指定
 data_file = "./images\ETL8G.pickle"
 im_size = 32
 out_size = 12075 # あーんまでの文字の数
-im_color = 3
+im_color = 1
 in_shape = (im_size, im_size, im_color)
 
 # 保存した画像データ一覧を読み込む
@@ -37,61 +33,45 @@ y = np.array(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_size=0.8, shuffle=True)
 
-# vgg16のインスタンスの生成
-input_tensor = Input(shape=(32, 32, 3))
-vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
-
-
 #モデル構築
-top_model = Sequential()
+model = Sequential()
+
 '''
-top_model.add(Conv2D(32, (3, 3), input_shape=in_shape))
-top_model.add(BatchNormalization())
-top_model.add(Activation('relu'))
-top_model.add(MaxPooling2D(pool_size=(2, 2)))
-top_model.add(Dropout(0.25))
+model.add(Conv2D(32, (3, 3), input_shape=in_shape))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-top_model.add(Conv2D(64, (3, 3)))
-top_model.add(BatchNormalization())
-top_model.add(Activation('relu'))
-top_model.add(Conv2D(64, (3, 3)))
-top_model.add(BatchNormalization())
-top_model.add(Activation('relu'))
-top_model.add(MaxPooling2D(pool_size=(2, 2)))
-top_model.add(Dropout(0.25))
-
-top_model.add(Flatten())
-top_model.add(Dense(512))
-top_model.add(BatchNormalization())
-top_model.add(Activation('relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(out_size))
-top_model.add(Activation('softmax'))
+model.add(Conv2D(64, (3, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 '''
 
-top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
-top_model.add(Dense(256, activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(2, activation='softmax'))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(out_size))
+model.add(Activation('softmax'))
 
-# モデルの連結
-model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer= RMSprop(),
+  metrics=['accuracy'])
 
-# vgg16の重みの固定
-for layer in model.layers[:19]:
-    layer.trainable = False
-
-top_model.compile(loss='categorical_crossentropy',
-              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-              metrics=['accuracy'])
-
-
-hist = top_model.fit(
+hist = model.fit(
   x_train, y_train,
-  batch_size=64, epochs=50,verbose=1,
+  batch_size=64, epochs=70,verbose=1,
   validation_data=(x_test, y_test))
 
-score = top_model.evaluate(x_test, y_test, verbose=1)
+score = model.evaluate(x_test, y_test, verbose=1)
 print("正解率 ", score[1], "loss ", score[0])
 
 plt.plot(hist.history['accuracy'])
@@ -106,4 +86,4 @@ plt.title('Loss')
 plt.legend(['train', 'test'], loc = 'upper left')
 plt.show()
 
-top_model.save('ETL8-model.h5')
+model.save('ETL8-model.h5')
